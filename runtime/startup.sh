77b9c9e0b40f651dd87feb86ed232b174df1b067
LilChicha174 <roman74mamin@gmail.com>
2024-10-08 00:57:39 +0300

    asgi 4 workers

--- runtime/startup.sh
+++ runtime/startup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-uvicorn delivery_test_task.app.main:app --host 0.0.0.0 --port 8000 --reload &
+uvicorn delivery_test_task.app.main:app --host 0.0.0.0 --port 8000 --workers 4 &
 celery --app=delivery_test_task.worker.celery_app worker --beat --loglevel=info &
 tail -f /dev/null
 
