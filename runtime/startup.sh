7b30f1164711f1b609a03be0eab626699519bfd0
LilChicha174 <roman74mamin@gmail.com>
2024-10-10 15:34:45 +0300

    simple test

--- runtime/startup.sh
+++ runtime/startup.sh
@@ -2,6 +2,11 @@
 
 uvicorn delivery_test_task.app.main:app --host 0.0.0.0 --port 8000 --workers 4 &
 celery --app=delivery_test_task.worker.celery_app worker --beat --loglevel=info &
+
+if [ "$1" = "tests" ]; then
+  pytest ./delivery_test_task/tests
+fi
+
 tail -f /dev/null
 
 
